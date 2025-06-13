a12b7537fc5f87131dadb4abb65dafcd1edd538e
Eduardo-David <eduardodavidlucaschoque36@gmail.com>
2025-06-13 10:27:53 -0400

    feat: añadiendo interfaz del vendedor a la app.

---
+++ gestionador-frontend/src/page/VendedroInterface.jsx
@@ -0,0 +1,8 @@
+function VendedorInterface(){
+    return(
+        <main className="bg-gray-600">
+            <h1 className="text-white font-sans">Vendedor Interface</h1>
+        </main>
+    )
+}
+export default VendedorInterface;
