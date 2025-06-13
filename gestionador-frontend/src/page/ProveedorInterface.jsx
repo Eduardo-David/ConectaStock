b20065465a83402b395570a255c6942c4b6ca59b
Eduardo-David <eduardodavidlucaschoque36@gmail.com>
2025-06-13 10:26:45 -0400

    feat: añadiendo interfaz del proveedor a la app.

---
+++ gestionador-frontend/src/page/ProveedorInterface.jsx
@@ -0,0 +1,9 @@
+
+function ProveedorInterface(){
+    return(
+        <main className="bg-gray-600">
+            <h1 className="text-white font-sans">Proveedor Interface</h1>
+        </main>
+    )
+}
+export default ProveedorInterface;
