b4a174fa530799b874d76f71f4af5f8cf64d30d6
Eduardo-David <eduardodavidlucaschoque36@gmail.com>
2025-07-23 16:30:45 -0400

    chore: ajustando diseño

--- gestionador-frontend/src/components/VendedorTabs.jsx
+++ gestionador-frontend/src/components/VendedorTabs.jsx
@@ -12,37 +12,24 @@ function AñadirProducto() {
                     <div class="grid grid-cols-2 gap-6">
                         <div class="col-span-2">
                             <label htmlFor="name" class="text-sm font-medium text-white">Nombre del Producto</label>
-                            <input type="text" name="name" id="name" class="mt-2 text-sm rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" required=""/>
+                            <input type="text" name="name" id="name" class="mt-2 text-sm rounded-lg w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white" placeholder="ingrese nombre del producto" required=""/>
                         </div>
                         <div class="w-full">
                             <label htmlFor="brand" class=" text-sm font-medium text-white">Marca</label>
-                            <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required=""/>
+                            <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="marca del producto" required=""/>
                         </div>
                         <div class="w-full">
                             <label htmlFor="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Precio</label>
-                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required=""/>
+                            <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="9999" required=""/>
                         </div>
                         <div>
                             <label htmlFor="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Categoria</label>
-                            <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                <option selected="">Select category</option>
-                                <option value="TV">TV/Monitors</option>
-                                <option value="PC">PC</option>
-                                <option value="GA">Gaming/Console</option>
-                                <option value="PH">Phones</option>
-                            </select>
+                            <input type="text" name="categoria" id="categoria" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="ingrese texto" required=""/>
                         </div>
-                        <div>
-                            <label htmlFor="item-weight" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Item Weight (kg)</label>
-                            <input type="number" name="item-weight" id="item-weight" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="12" required=""/>
-                        </div>
-                        <div class="sm:col-span-2">
-                            <label htmlFor="description" class="block text-sm font-medium text-white">Description</label>
-                            <textarea id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
-                        </div>
+                    
                     </div>
-                    <button type="submit" class="inline-flex items-center text-sm font-medium text-center text-white rounded-lg">
-                        Add product
+                    <button type="submit" class="inline-flex items-center text-sm font-medium text-center text-black bg-green-500 rounded-lg border-2 p-3 mt-2">
+                        Añadir Producto
                     </button>
                 </form>
            
